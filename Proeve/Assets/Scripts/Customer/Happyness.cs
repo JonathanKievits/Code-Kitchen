fef381608a4cbf4aebee8c7a7edb60f3f06bad5d
Yaruno <yarunoyo@outlook.com>
2020-04-17 15:50:05 +0200

    Error fixes, Happyness statemachine
    created a script that changes happyness states

---
+++ Proeve/Assets/Scripts/Customer/Happyness.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Happyness : MonoBehaviour
+{
+    //Variables
+    //Enum with the happyness states
+    public enum MoodState
+    {
+        Happy,
+        Neutral,
+        Angry
+    }
+
+    private MoodState _state;
+
+
+    //Functions
+
+    //Starts the state with happy
+    void Start()
+    {
+        _state = MoodState.Happy;    
+    }
+
+    //Raises the customer happyness
+    public void RaiseHappyness()
+    {
+        switch (_state)
+        {
+            case MoodState.Happy:
+                break;
+            case MoodState.Neutral:
+                _state = MoodState.Happy;
+                break;
+            case MoodState.Angry:
+                _state = MoodState.Neutral;
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Lowers the customer happyness
+    public void LowerHappyness()
+    {
+        switch (_state)
+        {
+            case MoodState.Happy:
+                _state = MoodState.Neutral;
+                break;
+            case MoodState.Neutral:
+                _state = MoodState.Angry;
+                break;
+            case MoodState.Angry:
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Changes the customer happyness with given state
+    public void SetHappyness(MoodState state)
+    {
+        _state = state;
+    }
+}
