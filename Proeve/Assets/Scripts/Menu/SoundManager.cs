189d8c403a0a46f960e9b78df0e3fbca1bd983c5
Yaruno <yarunoyo@outlook.com>
2020-05-11 14:36:15 +0200

    SoundManager
    vers 1 of sound manager. 
    You can call any sound registered in the manager from scripts and buttons

---
+++ Proeve/Assets/Scripts/Menu/SoundManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _audio;
+
+    //You cant serialize a dictionary. 
+    //So it has to be done with 2 arrays
+    [SerializeField]
+    private string[] _names;
+    [SerializeField]
+    private AudioClip[] _sounds;
+
+    public Dictionary<string, AudioClip> SoundDic;
+
+    void Start()
+    {
+        //Makes sure there is an audio source
+        if (_audio == null)
+        {
+            _audio = GetComponent<AudioSource>();
+        }
+
+        SoundDic = new Dictionary<string, AudioClip>();
+
+        //Adds sound to dictionary from arrays
+        for (int i = 0; i < _names.Length; i++)
+        {
+            SoundDic[_names[i]] = _sounds[i];
+        }
+    }
+
+    //Plays sound in dictionary with given name
+    public void PlaySound(string name)
+    {
+        //Makes sure the given name exists
+        for (int i = 0; i < _names.Length; i++)
+        {
+            if (name == _names[i])
+            {
+                _audio.clip = SoundDic[name];
+            }
+            else
+            {
+                Debug.LogWarning("Name does not exist");
+            }
+        }       
+
+        _audio.Play();
+    }
+}
