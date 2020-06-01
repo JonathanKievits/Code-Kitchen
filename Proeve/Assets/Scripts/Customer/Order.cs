35aec323c602000618448114331249f9531a2b24
DaveVanStaden <dave.v.staden@gmail.com>
2020-06-01 16:57:08 +0200

    Icecream added
    some fixes towards the order file, icecream added, new text bubble.

--- Proeve/Assets/Scripts/Customer/Order.cs
+++ Proeve/Assets/Scripts/Customer/Order.cs
@@ -9,9 +9,11 @@ public class Order : MonoBehaviour
     [SerializeField] private GameObject[] _meat = null;
     [SerializeField] private GameObject[] _sauce = null;
     [SerializeField] private GameObject[] _extra = null;
+    [SerializeField] private GameObject[] _iceFlavors = null;
     [SerializeField] private List<Transform> _orderHolder = null;
+    [SerializeField] private List<Transform> _iceHolder = null;
     Dictionary<string, GameObject> Burger = new Dictionary<string, GameObject>();
-    Dictionary<string, GameObject> Pizza = new Dictionary<string, GameObject>();
+    Dictionary<string, GameObject> Ice = new Dictionary<string, GameObject>();
     Dictionary<string, GameObject> Hotdog = new Dictionary<string, GameObject>();
     private bool _readyToOrder;
     private bool _chickenOrBeef = false;
@@ -24,7 +26,8 @@ public class Order : MonoBehaviour
         AddIngrdientsToList();
         //SpawnOrderBurger();
         //SpawnOrderHotDog();
-        GenerateOrder();
+        SpawnOrderIce();
+        //GenerateOrder();
     }
 
     //Generate order picks a rendom recipe and selects that. This is done so you wont allways have a burger. 
@@ -43,7 +46,7 @@ public class Order : MonoBehaviour
                     SpawnOrderHotDog();
                     break;
                 case 2:
-                    SpawnOrderPizza();
+                    SpawnOrderIce();
                     break;
             }
             
@@ -125,69 +128,42 @@ public class Order : MonoBehaviour
     }
 
     // in SpawnOrderPizza all pizza Ingredients are added in order. 
-    private void SpawnOrderPizza()
+    private void SpawnOrderIce()
     {
         // 2 ints are created to begin, 1 to place them in the right spot, 2 for a random extra ingrediënt. 
         int orderNumber = 0;
         int randomExtraIngredients = Random.Range(0, 3);
-        GameObject sauce = Instantiate(Pizza["ExtraSauce"], _orderHolder[orderNumber].position, Quaternion.identity);
-        sauce.transform.parent = _orderHolder[orderNumber];
-        sauce.name = "PizzaSauce";
+        GameObject icehorn = Instantiate(Ice["Horn"], _iceHolder[orderNumber].position, Quaternion.identity);
+        icehorn.transform.parent = _iceHolder[orderNumber];
+        icehorn.name = "IceHorn";
         orderNumber += 1;
-        for(int i = 0; i < randomExtraIngredients; i++)
+        switch (randomExtraIngredients)
         {
-            int randomIngredient = Random.Range(0, 4);
-            switch (randomIngredient)
-            {
-                case 0:
-                    GameObject mozarella = Instantiate(Pizza["Mozarella"], _orderHolder[orderNumber].position, Quaternion.identity);
-                    mozarella.transform.parent = _orderHolder[orderNumber];
-                    mozarella.name = "PizzaMozarella";
-                    orderNumber += 1;
-                    break;
-                case 1:
-                    GameObject pesto = Instantiate(Pizza["Pesto"], _orderHolder[orderNumber].position, Quaternion.identity);
-                    pesto.transform.parent = _orderHolder[orderNumber];
-                    pesto.name = "PizzaPesto";
-                    orderNumber += 1;
-                    break;
-                case 2:
-                    if (StaticK.Difficulty >= 1)
-                    {
-                        GameObject pineapple = Instantiate(Pizza["PineApple"], _orderHolder[orderNumber].position, Quaternion.identity);
-                        pineapple.transform.parent = _orderHolder[orderNumber];
-                        pineapple.name = "PizzaPineApple";
-                        orderNumber += 1;
-                    }
-                    else
-                    {
-                        i -= 1;
-                    }
-
-                    break;
-                case 3:
-                    if(StaticK.Difficulty >= 1)
-                    {
-                        GameObject salami = Instantiate(Pizza["Salami"], _orderHolder[orderNumber].position, Quaternion.identity);
-                        salami.transform.parent = _orderHolder[orderNumber];
-                        salami.name = "PizzaSalami";
-                        orderNumber += 1;
-                    }
-                    else
-                    {
-                        i -= 1;
-                    }
-
-                    break;
-            }
+            case 0:
+                GameObject banana = Instantiate(Ice["Banana"], _iceHolder[orderNumber].position, Quaternion.identity);
+                banana.transform.parent = _iceHolder[orderNumber];
+                banana.name = "IceBanana";
+                orderNumber += 1;
+                break;
+            case 1:
+                GameObject strawberry = Instantiate(Ice["Strawberry"], _iceHolder[orderNumber].position, Quaternion.identity);
+                strawberry.transform.parent = _iceHolder[orderNumber];
+                strawberry.name = "IceStrawberry";
+                orderNumber += 1;
+                break;
+            case 2:
+                GameObject chocolate = Instantiate(Ice["Chocolate"], _iceHolder[orderNumber].position, Quaternion.identity);
+                chocolate.transform.parent = _iceHolder[orderNumber];
+                chocolate.name = "IceChocolate";
+                orderNumber += 1;
+                break;
         }
-        GameObject tomato = Instantiate(Pizza["TomatoSauce"], _orderHolder[orderNumber].position, Quaternion.identity);
-        tomato.transform.parent = _orderHolder[orderNumber];
-        tomato.name = "PizzaTomato";
-        orderNumber += 1;
-        GameObject dough = Instantiate(Pizza["Dough"], _orderHolder[orderNumber].position, Quaternion.identity);
-        dough.transform.parent = _orderHolder[orderNumber];
-        dough.name = "PizzaDough";
+        if (StaticK.Difficulty >= 1)
+        {
+            GameObject cherry = Instantiate(Ice["Cherry"], _iceHolder[orderNumber].position, Quaternion.identity);
+            cherry.transform.parent = _iceHolder[orderNumber];
+            cherry.name = "IceCherry";
+        }
     }
 
     // in SpawnOrderHotDog all hotdog Ingredients are added in order. 
@@ -205,11 +181,7 @@ public class Order : MonoBehaviour
         meat.transform.parent = _orderHolder[orderNumber];
         meat.name = "HotdogMeat";
         orderNumber += 1;
-        GameObject sauceketchup = Instantiate(Hotdog["SauceKetchup"], _orderHolder[orderNumber].position, Quaternion.identity);
-        sauceketchup.transform.parent = _orderHolder[orderNumber];
-        sauceketchup.name = "HotdogSauce";
-        orderNumber += 1;
-        /*switch (randomSauce)
+        switch (randomSauce)
         {
             case 0:
                 GameObject sauceketchup = Instantiate(Hotdog["SauceKetchup"], _orderHolder[orderNumber].position, Quaternion.identity);
@@ -223,7 +195,7 @@ public class Order : MonoBehaviour
                 saucemustard.name = "HotdogSauce";
                 orderNumber += 1;
                 break;
-        }*/
+        }
         if (randomIngredient >= 2 && StaticK.Difficulty >= 1)
         {
             GameObject union = Instantiate(Hotdog["Union"], _orderHolder[orderNumber].position, Quaternion.identity);
@@ -247,28 +219,26 @@ public class Order : MonoBehaviour
         Burger.Add("TopBun", _baseRecipe[0]);
         Burger.Add("BottomBun", _baseRecipe[1]);
         Burger.Add("Patty", _meat[0]);
-        Burger.Add("Chicken", _meat[3]);
+        Burger.Add("Chicken", _meat[2]);
         Burger.Add("Sauce", _sauce[sauceNumber]);
-        Burger.Add("Cheese", _extra[4]);
-        Burger.Add("Union", _extra[5]);
+        Burger.Add("Cheese", _extra[1]);
+        Burger.Add("Union", _extra[2]);
         Burger.Add("Lettuce", _vegetables[0]);
         Burger.Add("Tomato", _vegetables[1]);
         Hotdog.Add("Union", _extra[0]);
         Hotdog.Add("SauceKetchup", _sauce[2]);
-        Hotdog.Add("SauceMustard", _sauce[6]);
+        Hotdog.Add("SauceMustard", _sauce[3]);
         Hotdog.Add("Meat", _meat[1]);
         Hotdog.Add("Bread", _baseRecipe[2]);
-        Pizza.Add("Dough", _baseRecipe[3]);
-        Pizza.Add("Salami", _meat[2]);
-        Pizza.Add("TomatoSauce", _sauce[3]);
-        Pizza.Add("ExtraSauce", _sauce[pizzaSauceNumber]);
-        Pizza.Add("Mozarella", _extra[1]);
-        Pizza.Add("Pesto", _extra[2]);
-        Pizza.Add("PineApple", _extra[3]);
+        Ice.Add("Horn", _baseRecipe[3]);
+        Ice.Add("Banana", _iceFlavors[0]);
+        Ice.Add("Strawberry", _iceFlavors[1]);
+        Ice.Add("Chocolate", _iceFlavors[2]);
+        Ice.Add("Cherry", _iceFlavors[3]);
     }
 
     // Under is a reset function so when the order is done, a new customer can post his order.
-    private void ResetOrder()
+    public void ResetOrder()
     {
         for(int i = 0; i < _orderHolder.Count; i++)
         {
